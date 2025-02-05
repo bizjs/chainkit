3cdca1b42a726ec1d0b85c11b7056157e29eeeaa
hstarorg <hm910705@163.com>
2025-02-05 14:41:17 +0800

    chore: upgrade deps

--- src/ChainSdkFactory.ts
+++ src/ChainSdkFactory.ts
@@ -8,51 +8,48 @@ import { SolanaChain } from './chain-sdks/SolanaChain';
 import { StarknetChain } from './chain-sdks/StarknetChain';
 import { TVMChain } from './chain-sdks/TVMChain';
 
-type ChainSdkResult<T extends ChainType> = T extends ChainType.Aptos
-  ? AptosChain
-  : T extends ChainType.Bitcoin
-  ? BitcoinChain
-  : T extends ChainType.Cardano
-  ? CardanoChain
-  : T extends ChainType.EVM
-  ? EvmChain
-  : T extends ChainType.Solana
-  ? SolanaChain
-  : T extends ChainType.Starknet
-  ? never
-  : T extends ChainType.TVM
-  ? TVMChain
-  : never;
+type ChainSdkMap = {
+  [ChainType.Aptos]: AptosChain;
+  [ChainType.Bitcoin]: BitcoinChain;
+  [ChainType.Cardano]: CardanoChain;
+  [ChainType.EVM]: EvmChain;
+  [ChainType.Solana]: SolanaChain;
+  [ChainType.Starknet]: StarknetChain;
+  [ChainType.TVM]: TVMChain;
+};
 
 export class ChainSdkFactory {
-  private static readonly _sdkCache: Partial<Record<ChainType, ChainSdkResult<ChainType>>> = {};
+  private static readonly _sdkMap: Partial<ChainSdkMap> = {};
 
-  static getChainSdk<T extends ChainType>(chainType: T): ChainSdkResult<T> {
-    switch (chainType) {
-      case ChainType.Aptos:
-        return this._getChainSdk(chainType, AptosChain);
-      case ChainType.Bitcoin:
-        return this._getChainSdk(chainType, BitcoinChain);
-      case ChainType.Cardano:
-        return this._getChainSdk(chainType, CardanoChain);
-      case ChainType.EVM:
-        return this._getChainSdk(chainType, EvmChain);
-      case ChainType.Solana:
-        return this._getChainSdk(chainType, SolanaChain);
-      case ChainType.Starknet:
-        return this._getChainSdk(chainType, StarknetChain);
-      case ChainType.TVM:
-        return this._getChainSdk(chainType, TVMChain);
+  static getChainSdk<T extends ChainType>(chainType: T) {
+    if (!this._sdkMap[chainType]) {
+      switch (chainType) {
+        case ChainType.Aptos:
+          this._sdkMap[ChainType.Aptos] = new AptosChain();
+          break;
+        case ChainType.Bitcoin:
+          this._sdkMap[ChainType.Bitcoin] = new BitcoinChain();
+          break;
+        case ChainType.Cardano:
+          this._sdkMap[ChainType.Cardano] = new CardanoChain();
+          break;
+        case ChainType.EVM:
+          this._sdkMap[ChainType.EVM] = new EvmChain();
+          break;
+        case ChainType.Solana:
+          this._sdkMap[ChainType.Solana] = new SolanaChain();
+          break;
+        case ChainType.Starknet:
+          this._sdkMap[ChainType.Starknet] = new StarknetChain();
+          break;
+        case ChainType.TVM:
+          this._sdkMap[ChainType.TVM] = new TVMChain();
+          break;
 
-      default:
-        throw new Error(`Chain type ${chainType} is not supported`);
+        default:
+          throw new Error(`Chain type ${chainType} is not supported`);
+      }
     }
-  }
-
-  private static _getChainSdk<T extends ChainType>(chainType: T, Ctor: { new () }): ChainSdkResult<T> {
-    if (!this._sdkCache[chainType]) {
-      this._sdkCache[chainType] = new Ctor();
-    }
-    return this._sdkCache[chainType] as ChainSdkResult<T>;
+    return this._sdkMap[chainType];
   }
 }
