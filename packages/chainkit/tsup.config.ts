21daca6fba21384466b2fd1dda3f513e7e4006cd
hstarorg <hm910705@163.com>
2025-05-07 17:30:55 +0800

    chore: add all in one package

---
+++ packages/chainkit/tsup.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: {
+    aptos: 'src/aptos.ts',
+    bitcoin: 'src/bitcoin.ts',
+    cardano: 'src/cardano.ts',
+    evm: 'src/evm.ts',
+    starknet: 'src/starknet.ts',
+    svm: 'src/svm.ts',
+    tvm: 'src/tvm.ts',
+  },
+  format: ['esm', 'cjs'],
+  dts: true,
+  sourcemap: false,
+  target: 'es2022',
+  clean: true,
+  treeshake: true,
+  legacyOutput: true,
+  noExternal: [],
+  esbuildOptions(options) {},
+});
