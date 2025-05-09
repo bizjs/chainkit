3f6fe6c4585d0c4a9ef06c3599503f7d7dbe6c54
hstarorg <hm910705@163.com>
2025-05-09 16:25:04 +0800

    feat(aptos): support public key input without 0x prefix

---
+++ .changeset/sharp-badgers-do.md
@@ -0,0 +1,5 @@
+---
+'@bizjs/chainkit-aptos': patch
+---
+
+feat:support public key input without 0x prefix
--- packages/aptos/src/utils/_internal.ts
+++ packages/aptos/src/utils/_internal.ts
@@ -1,4 +1,4 @@
 export function _getPublicKeyBuffer(publicKey: string): Buffer {
-  const key = publicKey.slice(2, 66);
+  const key = publicKey.indexOf('0x') === 0 ? publicKey.slice(2, 66) : publicKey;
   return Buffer.from(key, 'hex');
 }
